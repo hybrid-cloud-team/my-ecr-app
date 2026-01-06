23ecf3b6e475ddf10e966733b054d9c54808a74d
KimYejin69 <dada693832@gmail.com>
2026-01-06 12:47:02 +0900

    Update SqliteDesignTimeJellyfinDbFactory.cs

--- src/Jellyfin.Database/Jellyfin.Database.Providers.Sqlite/Migrations/SqliteDesignTimeJellyfinDbFactory.cs
+++ src/Jellyfin.Database/Jellyfin.Database.Providers.Sqlite/Migrations/SqliteDesignTimeJellyfinDbFactory.cs
@@ -1,15 +1,27 @@
+using Jellyfin.Database.Implementations;
+using Jellyfin.Database.Implementations.Locking;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Logging.Abstractions;
-using Jellyfin.Database.Implementations; // <--- 이 줄이 핵심입니다!
 
 namespace Jellyfin.Database.Providers.Sqlite.Migrations
 {
-    public class SqliteDesignTimeJellyfinDbFactory : IDesignTimeDbContextFactory<JellyfinDbContext>
+    /// <summary>
+    /// The design time factory for <see cref="JellyfinDbContext"/>.
+    /// This is only used for the creation of migrations and not during runtime.
+    /// </summary>
+    internal sealed class SqliteDesignTimeJellyfinDbFactory : IDesignTimeDbContextFactory<JellyfinDbContext>
     {
         public JellyfinDbContext CreateDbContext(string[] args)
         {
-            // 인자 값으로 NullLogger를 전달하여 생성자 규칙을 맞춥니다.
-            return new JellyfinDbContext(new SqliteDatabaseProvider(NullLogger<SqliteDatabaseProvider>.Instance));
+            var optionsBuilder = new DbContextOptionsBuilder<JellyfinDbContext>();
+            optionsBuilder.UseSqlite("Data Source=jellyfin.db", f => f.MigrationsAssembly(GetType().Assembly));
+
+            return new JellyfinDbContext(
+                optionsBuilder.Options,
+                NullLogger<JellyfinDbContext>.Instance,
+                new SqliteDatabaseProvider(null!, NullLogger<SqliteDatabaseProvider>.Instance),
+                new NoLockBehavior(NullLogger<NoLockBehavior>.Instance));
         }
     }
 }
