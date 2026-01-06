63e408496b5d42991ea2b4ad4c151557c0d48287
KimYejin69 <dada693832@gmail.com>
2026-01-06 12:42:54 +0900

    Update SqliteDesignTimeJellyfinDbFactory.cs

--- src/Jellyfin.Database/Jellyfin.Database.Providers.Sqlite/Migrations/SqliteDesignTimeJellyfinDbFactory.cs
+++ src/Jellyfin.Database/Jellyfin.Database.Providers.Sqlite/Migrations/SqliteDesignTimeJellyfinDbFactory.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Logging.Abstractions;
+using Jellyfin.Database.Implementations; // <--- 이 줄이 핵심입니다!
 
 namespace Jellyfin.Database.Providers.Sqlite.Migrations
 {
@@ -7,7 +8,7 @@ namespace Jellyfin.Database.Providers.Sqlite.Migrations
     {
         public JellyfinDbContext CreateDbContext(string[] args)
         {
-            // 아까 우리가 생성자를 바꿨기 때문에, 여기서도 인자를 맞춰줘야 합니다.
+            // 인자 값으로 NullLogger를 전달하여 생성자 규칙을 맞춥니다.
             return new JellyfinDbContext(new SqliteDatabaseProvider(NullLogger<SqliteDatabaseProvider>.Instance));
         }
     }
